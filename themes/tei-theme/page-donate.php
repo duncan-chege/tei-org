f9cd0db7870f02d2cfa9864891fe81825d96cd43
viisualworks <dshege4@gmail.com>
2025-08-25 14:08:04 +0300

    remove right padding on donate page

--- themes/tei-theme/page-donate.php
+++ themes/tei-theme/page-donate.php
@@ -12,7 +12,7 @@
 </section>
 
 <section class="lg:pr-12">
-    <div class="bg-mustard lg:pl-12 px-6 grid grid-cols-1 lg:grid-cols-2 lg:gap-8 gap-0 lg:my-16 my-6 items-center">
+    <div class="bg-mustard lg:pl-12 grid grid-cols-1 lg:grid-cols-2 lg:gap-8 gap-0 lg:my-16 my-6 items-center">
         <div class="space-y-6 my-8 lg:my-0">
             <h5 class="text-maroon">
                 DONOR CALL OUT
